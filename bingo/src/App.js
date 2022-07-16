f0a41f7b6d1bdbf03baba0bf80f42897d595de0f
Nico <nc3095@hotmail.com>
2022-07-15 22:11:39 -0300

    modifique app.js

--- bingo/src/App.js
+++ bingo/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola {nombre}, como andas?
+          Hola {nombre}, sos gay
         </p>
         <a
           className="App-link"
