218b44bc07f3c3dd735ad21439ce09389475b863
Nico <nc3095@hotmail.com>
2022-07-17 22:01:06 -0300

    agrego css grid, mapeo de la api

--- bingo/src/App.js
+++ bingo/src/App.js
@@ -1,30 +1,149 @@
-import logo from './logo.svg';
 import './App.css';
 const axios = require('axios');
 
 
 
-const apiUrl = 'http://localhost:5000/read'
-
-// Make a request for a user with a given ID
-axios.get('/user?ID=12345')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+//const apiUrl = 'http://localhost:5000/read'
 
 function App() {
+  const cajas =[
+    {
+      "id": 2,
+      "nombre": '"voy a hacer un té"',
+      "especial": 0
+    },
+    {
+      "id": 3,
+      "nombre": "maltrato animal",
+      "especial": 0
+    },
+    {
+      "id": 4,
+      "nombre": "carvu nword",
+      "especial": 0
+    },
+    {
+      "id": 5,
+      "nombre": "wea loca",
+      "especial": 0
+    },
+    {
+      "id": 6,
+      "nombre": "dani es gay",
+      "especial": 0
+    },
+    {
+      "id": 2,
+      "nombre": "voy a hacer un té",
+      "especial": 0
+    },
+    {
+      "id": 3,
+      "nombre": "maltrato animal",
+      "especial": 0
+    },
+    {
+      "id": 4,
+      "nombre": "carvu nword",
+      "especial": 0
+    },
+    {
+      "id": 5,
+      "nombre": "wea loca",
+      "especial": 0
+    },
+    {
+      "id": 6,
+      "nombre": "dani es gay",
+      "especial": 0
+    },
+    {
+      "id": 2,
+      "nombre": "voy a hacer un té",
+      "especial": 0
+    },
+    {
+      "id": 3,
+      "nombre": "maltrato animal",
+      "especial": 0
+    },
+    {
+      "id": 4,
+      "nombre": "carvu NO nword",
+      "especial": 1
+    },
+    {
+      "id": 5,
+      "nombre": "wea loca",
+      "especial": 0
+    },
+    {
+      "id": 6,
+      "nombre": "dani es gay",
+      "especial": 0
+    },
+    {
+      "id": 2,
+      "nombre": "voy a hacer un té",
+      "especial": 0
+    },
+    {
+      "id": 3,
+      "nombre": "maltrato animal",
+      "especial": 0
+    },
+    {
+      "id": 4,
+      "nombre": "carvu nword",
+      "especial": 0
+    },
+    {
+      "id": 5,
+      "nombre": "wea loca",
+      "especial": 0
+    },
+    {
+      "id": 6,
+      "nombre": "dani es gay",
+      "especial": 0
+    },
+    {
+      "id": 2,
+      "nombre": "voy a hacer un té",
+      "especial": 0
+    },
+    {
+      "id": 3,
+      "nombre": "maltrato animal",
+      "especial": 0
+    },
+    {
+      "id": 4,
+      "nombre": "carvu nword",
+      "especial": 0
+    },
+    {
+      "id": 5,
+      "nombre": "wea loca",
+      "especial": 0
+    },
+    {
+      "id": 6,
+      "nombre": "dani es gay",
+      "especial": 0
+    },
+  ];
   return (
-
-    
-
+    <>
+      <h1>Mi bingo</h1>
+      <section className="container">
+        {cajas.map((caja) => (
+          <article className={caja.especial === 1 ? "caja caja-especial" : "caja caja-normal"} key={caja.id}>
+            {caja.nombre}
+          </article>
+        ))}
+      </section>
+    </>
   );
 }
 
